1191a413d483767b900d99ee0b701b2b234cdcc9
Abdurrahman Rahim <imanabdurrahmanr@gmail.com>
2023-08-19 14:08:57 +0800

    fix carousel

--- src/pages/beranda/karya/Karya.jsx
+++ src/pages/beranda/karya/Karya.jsx
@@ -19,7 +19,7 @@ const Karya = () => {
                     <Button title='Selengkapnya' link='/karya' />
                 </div>
             </div>
-            <div className='relative w-full lg:col-span-3 col-span-full overflow-x-hidden'>
+            <div className='relative w-full lg:col-span-3 col-span-full'>
                 <Swiper
                     loop={true}
                     navigation={true}
@@ -31,20 +31,6 @@ const Karya = () => {
                         el: '.pagination',
                     }}
                     oneWayMovement={true}
-                    breakpoints={{
-                        0: {
-                            slidesPerView: 1,
-                            spaceBetween: 0
-                        },
-                        768: {
-                            slidesPerView: 2,
-                            spaceBetween: 12
-                        },
-                        1024: {
-                            slidesPerView: 2,
-                            spaceBetween: 24
-                        }
-                    }}
                     creativeEffect={{
                         limitProgress: 2,
                         prev: {
@@ -60,10 +46,10 @@ const Karya = () => {
                     }}
                     slidesPerView={2}
                     spaceBetween={24}
-                    // autoplay={{
-                    //     delay: 1000,
-                    //     disableOnInteraction: false,
-                    // }}
+                    autoplay={{
+                        delay: 1000,
+                        disableOnInteraction: false,
+                    }}
                     speed={1200}
                     className="mySwipe"
                 >
@@ -72,14 +58,16 @@ const Karya = () => {
                             <SwiperSlide key={data} className={`flex items-center h-full relative`}>
                                 {({ isActive }) => (
                                     <>
-                                        <div className={`text-primary max-w-[404px] max-h-[582px] rounded-[20px] overflow-hidden transition-all delay-500`}>
-                                            <img src={'https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&quality=85&auto=format&fit=max&s=a52bbe202f57ac0f5ff7f47166906403'} alt="" className='object-cover h-full w-full' />
+                                        <div className={`text-primary w-[404px] h-[582px] rounded-[20px] overflow-hidden transition-all delay-500`}>
+                                            <img src={'https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&quality=85&auto=format&fit=max&s=a52bbe202f57ac0f5ff7f47166906403'} alt="" className='object-cover h-full' />
                                         </div>
-                                        <div className={`${isActive ? 'opacity-100 -translate-y-7 translate-x-7' : 'opacity-0'} absolute bg-white bottom-0 left-0 rounded-[10px] py-6 w-[217px] text-secondary flex flex-col gap-3 backdrop-blur-[1.5px] bg-opacity-75 transition-all duration-1000`}>
-                                            <p className='text-xs font-medium text-greyCol'>Website</p>
-                                            <p className='text-xs font-semibold'>Karya terbaik</p>
-                                            <div className='absolute w-12 h-12 -right-6 -bottom-2 bg-primary rounded-[10px] flex items-center justify-center'>
-                                                <img src={arrowRight} alt="" />
+                                        <div className={`${isActive ? 'opacity-100 -translate-y-7 translate-x-7' : 'opacity-0'} absolute bg-white bottom-0 left-0 rounded-[10px] py-6 w-[217px] text-secondary backdrop-blur-[1.5px] bg-opacity-75 transition-all duration-1000`}>
+                                            <div className='flex flex-col gap-3 ml-7'>
+                                                <p className='text-xs font-medium text-greyCol'>Website</p>
+                                                <p className='text-xs font-semibold'>Karya terbaik</p>
+                                                <div className='absolute w-12 h-12 -right-6 -bottom-2 bg-primary rounded-[10px] flex items-center justify-center'>
+                                                    <img src={arrowRight} alt="" />
+                                                </div>
                                             </div>
                                         </div>
                                     </>
