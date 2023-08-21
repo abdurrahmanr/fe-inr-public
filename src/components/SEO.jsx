f2ec7b3b762545dbdbc3f220c2dcd15a9c5696fb
Abdurrahman Rahim <imanabdurrahmanr@gmail.com>
2023-08-21 20:13:31 +0800

    feat: trying to fix SEO

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -1,23 +1,23 @@
 import { Helmet } from 'react-helmet-async';
 
-const SEOComponent = ({ title, description, name, type, image }) => {
+const SEOComponent = ({ title }) => {
     return (
         <Helmet>
             { /* Standard metadata tags */}
             <title>{title}</title>
-            <meta name='description' content={description} />
+            {/* <meta name='description' content={description} /> */}
             { /* End standard metadata tags */}
             { /* Facebook tags */}
-            <meta property="og:type" content={type} />
+            {/* <meta property="og:type" content={type} />
             <meta property="og:title" content={title} />
             <meta property="og:description" content={description} />
-            <meta property="og:image" content={image} />
+            <meta property="og:image" content={image} /> */}
             { /* End Facebook tags */}
             { /* Twitter tags */}
-            <meta name="twitter:creator" content={name} />
+            {/* <meta name="twitter:creator" content={name} />
             <meta name="twitter:card" content={type} />
             <meta name="twitter:title" content={title} />
-            <meta name="twitter:description" content={description} />
+            <meta name="twitter:description" content={description} /> */}
             { /* End Twitter tags */}
         </Helmet>
     );
