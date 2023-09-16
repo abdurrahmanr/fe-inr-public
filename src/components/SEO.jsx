3db2ee2aaa3c13ff9244e7441985dceb3875b0da
abdurrahmanr <imanabdurrahmanr@gmail.com>
2023-09-17 00:31:34 +0800

    feat: trying to fix SEO

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -5,12 +5,12 @@ const SEOComponent = ({ title, description }) => {
 		<Helmet>
 			{/* Standard metadata tags */}
 			<title>{title}</title>
-			{/* <meta name='description' content={description} /> */}
+			<meta name='description' content={description} />
 			{/* End standard metadata tags */}
 			{/* Facebook tags */}
 			{/* <meta property="og:type" content={type} /> */}
 			<meta property="og:title" content={title} />
-			{/* <meta property="og:description" content={description} /> */}
+			<meta property="og:description" content={description} />
 			{/* <meta property="og:image" content={image} /> */}
 			{/* End Facebook tags */}
 			{/* Twitter tags */}
