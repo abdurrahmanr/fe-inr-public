4ceabfc8a001f91cb455ab1b4c95ef161e39607e
abdurrahmanr <imanabdurrahmanr@gmail.com>
2023-09-17 00:27:53 +0800

    feat: trying to fix SEO

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -1,6 +1,6 @@
 import { Helmet } from 'react-helmet-async';
 
-const SEOComponent = ({ title }) => {
+const SEOComponent = ({ title, description }) => {
 	return (
 		<Helmet>
 			{/* Standard metadata tags */}
@@ -8,16 +8,16 @@ const SEOComponent = ({ title }) => {
 			{/* <meta name='description' content={description} /> */}
 			{/* End standard metadata tags */}
 			{/* Facebook tags */}
-			{/* <meta property="og:type" content={type} />
-            <meta property="og:title" content={title} />
-            <meta property="og:description" content={description} />
-            <meta property="og:image" content={image} /> */}
+			<meta property="og:type" content={type} />
+			<meta property="og:title" content={title} />
+			<meta property="og:description" content={description} />
+			<meta property="og:image" content={image} />
 			{/* End Facebook tags */}
 			{/* Twitter tags */}
-			{/* <meta name="twitter:creator" content={name} />
-            <meta name="twitter:card" content={type} />
-            <meta name="twitter:title" content={title} />
-            <meta name="twitter:description" content={description} /> */}
+			<meta name="twitter:creator" content={name} />
+			<meta name="twitter:card" content={type} />
+			<meta name="twitter:title" content={title} />
+			<meta name="twitter:description" content={description} />
 			{/* End Twitter tags */}
 		</Helmet>
 	);
